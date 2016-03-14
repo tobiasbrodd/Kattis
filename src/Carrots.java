405660563c4aa4aaacd62137f114d6788ed71c40
Tobias Brodd <tbrodd@kth.se>
2016-03-14 12:24:12 +0100

    Initial commit

---
+++ src/Carrots.java
@@ -0,0 +1,13 @@
+/**
+ * Created by tobbe on 2016-03-14.
+ *
+ * @author tobbe
+ * @version 2016.03.14
+ */
+public class Carrots {
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        io.getInt();
+        System.out.println(io.getInt());
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by tobbe on 2016-03-14.
+ *
+ * @author tobbe
+ * @version 2016.03.14
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
